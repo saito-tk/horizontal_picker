64de1a75a3adf9b48a1f367f7721061bcb98a348
saito-tk <durath117@gmail.com>
2026-02-20 18:05:19 +0900

    ツールバーの下に画面が潜らないようにする

--- sample/src/main/java/com/saitotk/horizontalpicker/sample/MainActivity.kt
+++ sample/src/main/java/com/saitotk/horizontalpicker/sample/MainActivity.kt
@@ -9,10 +9,13 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.safeDrawing
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.verticalScroll
@@ -60,6 +63,7 @@ private fun SampleScreen() {
     Column(
         modifier = Modifier
             .fillMaxSize()
+            .windowInsetsPadding(WindowInsets.safeDrawing)
             .background(
                 Brush.verticalGradient(
                     colors = listOf(
